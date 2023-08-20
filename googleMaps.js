49a84ac3fd1137f4e24ccda33cbe3f1de6495c26
ataraxielle <yoakenozero@gmail.com>
2023-08-20 22:56:50 +0800

    with maps page

--- googleMaps.js
+++ googleMaps.js
@@ -1,26 +1,69 @@
-const queryParams = new URLSearchParams(window.location.search);
-const countryName = queryParams.get("name");
+document.addEventListener("DOMContentLoaded", () => {
+  const countryTable = document.getElementById("countryTable");
+  const mapDiv = document.getElementById("map");
+  const searchInput = document.getElementById("searchInput");
+  let map = null;
 
-fetch(`https://restcountries.com/v3.1/name/${encodeURIComponent(countryName)}`)
-  .then(response => response.json())
-  .then(data => {
-    const country = data[0]; // Extracting country data from the response
-    const mapsContainer = document.getElementById('maps-container');
+  // Fetch country data from the API
+  fetch("https://restcountries.com/v3.1/all")
+    .then(response => response.json())
+    .then(data => {
+      const countries = data.map(country => country.name.common);
 
-    const [latitude, longitude] = country.latlng;
+      // Populate the table with country names
+      countries.forEach(countryName => {
+        const row = countryTable.insertRow();
+        const cell = row.insertCell();
+        cell.textContent = countryName;
+        cell.addEventListener("click", () => displayMap(countryName));
+      });
 
-    // Create a Leaflet map centered on the specified latitude and longitude
-    const map = L.map('maps-container').setView([latitude, longitude], 5);
+      // Add event listener for search input
+      searchInput.addEventListener("input", () => filterCountries(countries));
+    })
+    .catch(error => console.error("Error fetching data:", error));
 
-    // Add a tile layer to the map (you can choose a different tile layer if you prefer)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  // Display the map for the selected country
+  function displayMap(countryName) {
+    if (map) {
+      map.remove();
+    }
+  
+    mapDiv.innerHTML = "";
+    map = L.map("map").setView([0, 0], 2); 
+  
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; OpenStreetMap contributors"
     }).addTo(map);
+  
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+      .then(response => response.json())
+      .then(data => {
+        const country = data[0];
+        if (country && country.latlng) {
+          const [lat, lng] = country.latlng;
+  
+          map.setView([lat, lng], 6); 
+          L.marker([lat, lng]).addTo(map);
+        } else {
+          console.error("No coordinates found for the selected country.");
+        }
+      })
+      .catch(error => console.error("Error fetching country data:", error));
+  }
 
-    // Add a marker to the map at the specified latitude and longitude
-    L.marker([latitude, longitude]).addTo(map)
-      .bindPopup(`<b>${countryName}</b><br>Latitude: ${latitude}<br>Longitude: ${longitude}`)
-      .openPopup();
-  })
-  .catch(error => console.error('Error fetching country data:', error));
+  // Filter countries based on search input
+  function filterCountries(countries) {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredCountries = countries.filter(country => country.toLowerCase().includes(searchTerm));
+
+    countryTable.innerHTML = ""; // Clear the table
+
+    filteredCountries.forEach(countryName => {
+      const row = countryTable.insertRow();
+      const cell = row.insertCell();
+      cell.textContent = countryName;
+      cell.addEventListener("click", () => displayMap(countryName));
+    });
+  }
+});
